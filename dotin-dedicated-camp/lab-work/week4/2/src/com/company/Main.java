242bf50fdb1e5eeb2580240b8b74bfa1cafb4006
delaram <delaramrabiee72@gmail.com>
2021-08-17 19:23:03 -0700

    commit

---
+++ dotin-dedicated-camp/lab-work/week4/2/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] a=new int[5];
+        Scanner s=new Scanner(System.in);
+        try{
+            for (int i = 0; i <6 ; i++) {
+                System.out.println("pls enter a num");
+                a[i] = s.nextInt();
+            }
+        }catch (Exception e){
+            System.out.println("error");
+        }
+
+
+    }
+}
