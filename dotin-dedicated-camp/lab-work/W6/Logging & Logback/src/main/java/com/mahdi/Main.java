da50fefd3a88db0dbc665e58bfd53403cf7fa2b5
Mahdi Ghomsheh <kitkat.helix@gmail.com>
2021-09-05 02:42:37 +0430

    W6 exercise

---
+++ dotin-dedicated-camp/lab-work/W6/Logging & Logback/src/main/java/com/mahdi/Main.java
@@ -0,0 +1,15 @@
+package com.mahdi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SimpleLogger simpleLogger = new SimpleLogger();
+        simpleLogger.logging();
+
+        LogBack logBack = new LogBack();
+        logBack.logging();
+
+    }
+
+}
