ea00d3f4e0df8296dfa2e21633791ecca938ce9d
Your Name <you@example.com>
2021-09-16 23:29:51 +0430

    add simple orm project

--- dotin-dedicated-camp/lab-work/week8/simpleORM/src/main/java/com/mapsa/orm/main/Main.java
+++ dotin-dedicated-camp/lab-work/week8/simpleORM/src/main/java/com/mapsa/orm/main/Main.java
@@ -1,8 +1,5 @@
 package com.mapsa.orm.main;
 
-import com.mapsa.orm.database.CRUDGenerator;
-import com.mapsa.orm.model.Person;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
