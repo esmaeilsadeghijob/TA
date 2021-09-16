1ca9d99ec5ae7508b663aac03ba9b140649afce4
fatemehbatooei <88035959+fatemehbatooei@users.noreply.github.com>
2021-09-16 23:29:01 +0430

    Update Main.java

--- dotin-dedicated-camp/lab-work/week8/simpleORM/src/main/java/com/mapsa/orm/main/Main.java
+++ dotin-dedicated-camp/lab-work/week8/simpleORM/src/main/java/com/mapsa/orm/main/Main.java
@@ -11,17 +11,5 @@ public class Main {
         execute.executeApplication();
 
     }
-
-   /* CRUDGenerator<Person> generator = new CRUDGenerator();
-    //create table
-    //generator.create(new Person());
-
-    //insert
-    Person person1 = new Person().setName("fatemeh1").setFamily("batooei1");
-        generator.save(person1);
-
-    //delete
-    //generator.save(new Person().setId(1));*/
-
 }
 
